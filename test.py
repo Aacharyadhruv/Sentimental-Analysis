ac1fda1eb00d0b39afbfac803be36609972ed356
Aacharyadhruv <162418116+Aacharyadhruv@users.noreply.github.com>
2024-05-09 12:16:37 +0530

    Update test.py

--- test.py
+++ test.py
@@ -83,6 +83,7 @@ def main():
             else:
                 text_processor = TextProcessor()
                 input_sms = text_processor.clean_text(input_sms)
+                st.write(type(input_sms))
                 sentiment = TextBlob("Not Happy").sentiment
                 # sentiment = SentimentAnalyzer.generate_polarity(input_sms)
                 st.header(sentiment[0])
